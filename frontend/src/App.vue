6aa44a8f8ac09a2f3b0bc3e5fd1028cc12c49660
kkarlo23 <maroskarlo2312@gmail.com>
2022-12-11 15:45:03 +0100

    frontend basic structure added

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterView } from "vue-router";
+import HeaderComponent from "./components/Header.vue";
+</script>
+
+<template>
+  <div class="main-container">
+    <HeaderComponent title="bSocial" />
+    <div class="main-content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  min-height: 100vw;
+  min-width: 100vw;
+  background-color: rgb(235, 235, 235);
+}
+.main-content {
+  padding: 20px;
+}
+</style>
---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="search-container">
+      <input
+        class="search-input"
+        placeholder="Search for user..."
+        type="text"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainView",
+  props: {
+    title: String,
+  },
+  setup() {},
+});
+</script>
+
+<style>
+.search-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.search-input {
+  width: 300px;
+  height: 35px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding: 10px;
+  outline: none;
+  border: 0;
+}
+</style>
