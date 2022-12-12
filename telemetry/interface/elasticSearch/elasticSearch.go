621dbddfea689e5d010a06077f66f8102c8836a6
KarloMarosAG04 <karlo.maros@ag04.com>
2022-12-12 14:13:01 +0100

    added config files

--- telemetry/interface/elasticSearch/elasticSearch.go
+++ telemetry/interface/elasticSearch/elasticSearch.go
@@ -12,31 +12,6 @@ import (
 // var ESClient *elasticsearch.Client
 var ESClient *elastic.Client
 
-// func InitESClient() error {
-// 	cfg := elasticsearch.Config{
-// 		Addresses: []string{
-// 			fmt.Sprintf("%s://%s:%s",
-// 				helpers.CONFIG.ElasticSearch.ElasticSearchProtocol,
-// 				helpers.CONFIG.ElasticSearch.ElasticSearchHost,
-// 				helpers.CONFIG.ElasticSearch.ElasticSearchPort),
-// 		},
-// 		// ...
-// 	}
-// 	es, err := elasticsearch.NewClient(cfg)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	ESClient = es
-// 	return nil
-// 	// res, err := es.Info()
-// 	// if err != nil {
-// 	// 	log.Fatalf("Error getting response: %s", err)
-// 	// }
-
-//		// defer res.Body.Close()
-//		// log.Println(res)
-//	}
-
 func InitESClient() error {
 
 	client, err := elastic.NewClient(elastic.SetURL(fmt.Sprintf("%s://%s:%s",
